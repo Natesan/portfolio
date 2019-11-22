717e0b038c9f3a7a182d04f69d487ebec0e9f99f
natesan.sivagnanam <natesan.sivagnanam@sap.com>
2019-11-22 18:01:34 +0530

    Updates to Page Content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,11 +3,31 @@ import Container from "../components/container"
 import Header from "../components/header"
 import Layout from "../components/layout"
 
+import { IoMdMail, IoLogoTwitter } from "react-icons/io"
+import { MdPhoneInTalk } from "react-icons/md"
+
 export default () => (
   <Container>
     <Layout>
-      <Header headerText="Contact" />
-      <p>Work in Progress!</p>
+      <Header headerText="Don't Be Shy, Say Hi!" />
+      <div className="panel panel-default pt-2 text-center">
+        <p>Go on come on!</p>
+      </div>
+      <div className="panel panel-default pt-5 text-center d-flex justify-content-around">
+        <a href="mailto:nmsnatesan@gmail.com?Subject=Ahoy%20Hoy" target="_top">
+          <IoMdMail fontSize="40px" />
+        </a>
+        <a href="tel:+919742398411">
+          <MdPhoneInTalk fontSize="40px" />
+        </a>
+        <a
+          href="https://www.twitter.com/NatesanSiv"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <IoLogoTwitter fontSize="40px" />
+        </a>
+      </div>
     </Layout>
   </Container>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,42 +15,47 @@ import {
 export default () => (
   <Container>
     <Layout>
-      <div class="jumbotron text-center my-3">
+      <div className="jumbotron text-center my-3">
         <Header headerText="Hello! I'm Natesan"></Header>
       </div>
-      <div class="panel panel-default pt-2 text-center">
+      <div className="panel panel-default pt-2 text-center">
         <p>
           Computer Science graduate from Coonoor. Living in Bangalore, Product
           UI development is what I do. Love to travel, photograph, trek and
           read. I play a whole lot of games and follow a bunch of sports.
         </p>
       </div>
-      <div class="panel panel-default pt-5 text-center d-flex justify-content-around">
+      <div className="panel panel-default pt-5 text-center d-flex justify-content-around">
         <IoLogoLinkedin
           onClick={() =>
             window.open("https://www.linkedin.com/in/natesansivagnanam")
           }
           fontSize="60px"
+          className="cursor"
         />
         <IoLogoGithub
           onClick={() => window.open("https://github.com/Natesan")}
           fontSize="60px"
+          className="cursor"
         />
         <IoLogoTwitter
           onClick={() => window.open("https://www.twitter.com/NatesanSiv")}
           fontSize="60px"
+          className="cursor"
         />
         <IoLogoInstagram
           onClick={() =>
             window.open("https://www.instagram.com/natesan.sivagnanam")
           }
           fontSize="60px"
+          className="cursor"
         />
         <IoLogoFacebook
           onClick={() =>
             window.open("https://www.facebook.com/Natesan.Sivagnanam")
           }
           fontSize="60px"
+          className="cursor"
         />
       </div>
     </Layout>
